e11a495ec70fec6cd5abebbebddc435d8c31d901
David Markel <davidgmarkel@gmail.com>
2018-03-27 15:52:14 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,5 @@
 class Project
-attr_accessor :title, :backers
+attr_accessor :title, :backers, :backed_projects
 
   def initialize(title)
     @title = title
@@ -8,6 +8,7 @@ attr_accessor :title, :backers
 
   def add_backer(backer)
     @backers << backer
+    @backed_projects << self
   end
 
 end
